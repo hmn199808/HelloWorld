51c98afbd949dd2a4473545e1f9c26982f168faa
houmengnan <2576527470@qq.com>
2024-04-09 15:49:14 +0800

    Revert "实现了排序功能"
    This reverts commit d023af3e

--- sort.cpp
+++ sort.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
 using namespace std;
-// 阿亮把注释删除
+
 int main() 
 {
-    // 小张把注释删除
     实现了sort
-        小张通过快速实现
-        456
     return 0;
 }
-
-
-
-sortdev实现希尔排序
-
-
-强制通过本地的另一个分支sortdev提交到远程
-
-
-测试reset
